ed514d12b3bc3ded92a6fbcaf4b4e1ebe6e7928f
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-25 15:24:02 -0600

    setting up migrations

---
+++ alembic/versions/20201225_151938_the_message_about_the_revision.py
@@ -0,0 +1,24 @@
+"""the message about the revision
+
+Revision ID: 899f4f2c3a3f
+Revises: 
+Create Date: 2020-12-25 15:19:38.611080
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '899f4f2c3a3f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
